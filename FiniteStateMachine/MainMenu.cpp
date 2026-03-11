88091a19cc91cba927928ebe748b16dcdab9333d
hleroyer248 <hleroyer@gaming.tech>
2026-03-11 11:44:29 +0100

    correction sauvegarde

--- FiniteStateMachine/MainMenu.cpp
+++ FiniteStateMachine/MainMenu.cpp
@@ -6,27 +6,29 @@ MainMenu::MainMenu(const RessourcesManager& rm)
     settingsBtn(rm.getSettingsBtnTexture()),
     exitBtn(rm.getExitBtnTexture()),
     title(rm.getFont(), "FLAPPY BIRD", 50),
-    shopButton(rm.getShopTexture()), 
-    bestScoreText(rm.getFont(), "", 24),    
-    totalScoreText(rm.getFont(), "", 24) {
+    shopButton(rm.getShopTexture()), // commit sauvegarde 
+    bestScoreText(rm.getFont(), "", 24),    // commit sauvegarde
+    totalScoreText(rm.getFont(), "", 24)    // commit sauvegarde
+{
 
     title.setPosition({ 230.f, 100.f });
     title.setFillColor(sf::Color::White);
 
-    // Positions pour centrer les boutons (à ajuster selon la taille de tes images)
     startBtn.setPosition({ 300.f, 250.f });
     settingsBtn.setPosition({ 300.f, 350.f });
     exitBtn.setPosition({ 300.f, 450.f });
     shopButton.setPosition({ 300.f, 400.f });
-    shopButton.setScale({1.f,1.f});
+    shopButton.setScale({ 1.f,1.f });
 
-        bestScoreText.setFillColor(sf::Color::Red);
+    // commit sauvegarde 
+    bestScoreText.setFillColor(sf::Color::Red);
     bestScoreText.setPosition({ 300.f, 20.f });
 
     totalScoreText.setFillColor(sf::Color::Green);
     totalScoreText.setPosition({ 10.f, 10.f });
 }
 
+// commit sauvegarde 
 void MainMenu::updateScores(int bestScore, int totalScore) {
     bestScoreText.setString("Meilleur Score: " + std::to_string(bestScore));
     totalScoreText.setString("Score Total: " + std::to_string(totalScore));
@@ -53,6 +55,8 @@ void MainMenu::draw(sf::RenderWindow& window) const {
     window.draw(settingsBtn);
     window.draw(exitBtn);
     window.draw(shopButton);
+
+    // commit sauvegarde 
     window.draw(bestScoreText);
     window.draw(totalScoreText);
 }
