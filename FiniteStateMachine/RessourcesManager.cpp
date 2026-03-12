e0e1e5ec11e58323aadc2cf59a7e50ed5bdf4a25
leo bouyat <lbouyat@gaming.tech>
2026-03-12 11:00:36 +0100

    Skin principale player

--- FiniteStateMachine/RessourcesManager.cpp
+++ FiniteStateMachine/RessourcesManager.cpp
@@ -29,10 +29,14 @@ bool RessourcesManager::loadAll() {
         !birdBlueTex.loadFromFile(basePath + "BirdBlue.png") ||
         !birdGreenTex.loadFromFile(basePath + "BirdGreen.png") ||
         !birdGoldTex.loadFromFile(basePath + "BirdGold.png") ||
+        !defaultBirdTex.loadFromFile(basePath + "defaultBird.jpeg") ||
         !birdShadowTex.loadFromFile(basePath + "BirdShadow.png")){
         std::cerr << "ERREUR: Impossible de charger un ou plusieurs assets png.\n";
         return false;
     }
+    std::cout << "DefaultBird size: "
+        << defaultBirdTex.getSize().x << " "
+        << defaultBirdTex.getSize().y << std::endl;
     return true;
 }
 
@@ -41,7 +45,6 @@ const sf::Texture& RessourcesManager::getBgTexture() const { return bgTex; }
 const sf::Texture& RessourcesManager::getTopPipeTexture() const { return topPipeTex; }
 const sf::Texture& RessourcesManager::getBottomPipeTexture() const { return bottomPipeTex; }
 const sf::Font& RessourcesManager::getFont() const { return font; }
-
 const sf::Texture& RessourcesManager::getMenuBgTexture() const { return menuBgTex; }
 const sf::Texture& RessourcesManager::getStartBtnTexture() const { return startBtnTex; }
 const sf::Texture& RessourcesManager::getSettingsBtnTexture() const { return settingsBtnTex; }
@@ -53,10 +56,9 @@ const sf::Texture& RessourcesManager::getBuyBtnTexture() const { return buyBtnTe
 const sf::Texture& RessourcesManager::getEquipBtnTexture() const { return equipBtnTex; }
 const sf::Texture& RessourcesManager::getEquippedBtnTexture() const { return equippedBtnTex; }
 const sf::Texture& RessourcesManager::getGhostPlayerTexture() const { return ghostPlayerTex; } // Commit Ghost
-
-
 const sf::Texture& RessourcesManager::getBirdRedTexture() const{ return birdRedTex; }
 const sf::Texture& RessourcesManager::getBirdBlueTexture() const{ return birdBlueTex; }
 const sf::Texture& RessourcesManager::getBirdGreenTexture() const{ return birdGreenTex; }
 const sf::Texture& RessourcesManager::getBirdGoldTexture() const{ return birdGoldTex; }
 const sf::Texture& RessourcesManager::getBirdShadowTexture() const{ return birdShadowTex; }
+const sf::Texture& RessourcesManager::getDefaultBirdTexture() const{ return defaultBirdTex; }
