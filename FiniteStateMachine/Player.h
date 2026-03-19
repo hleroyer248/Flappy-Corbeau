d9d944fc8648675f534261ad563ceec3485824c8
hleroyer248 <hleroyer@gaming.tech>
2026-03-19 16:31:17 +0100

    Update Player.h

--- FiniteStateMachine/Player.h
+++ FiniteStateMachine/Player.h
@@ -10,17 +10,14 @@ public:
     void flap();
     void update(float dt);
 
-    // Commit Ghost - debut 
     void activateGhost();
     void updateGhost(float dt);
     bool isGhost() const;
     bool canActivateGhost() const;
-    // Commit Ghost - fin
 
     CollisionBox getCollisionBox() const;
     const sf::Sprite& getSprite() const;
 
-    // Correction SFML 3.0 : Retourne une valeur et non une référence
     sf::Vector2f getPosition() const;
 
     void setSkin(const sf::Texture& texture);
@@ -33,7 +30,6 @@ private:
     sf::Sprite sprite;
     sf::Vector2f velocity;
 
-    // Commit Ghost - debut
     const sf::Texture* normalTexture;
     const sf::Texture* ghostTexture;
 
@@ -42,10 +38,8 @@ private:
     float ghostCooldownTimer;
     const float GHOST_DURATION = 5.0f;
     const float GHOST_COOLDOWN_MAX = 6.0f;
-    // Commit Ghost - fin
 
-    // Commit Crow - debut
-    const sf::Texture* defaultPlayerTexture; // Pour identifier l'image de base animée
+    const sf::Texture* defaultPlayerTexture; 
     bool isAnimated;
     int currentFrame;
     float animationTimer;
@@ -55,13 +49,10 @@ private:
     int totalFrames;
 
     void applyCurrentTexture(const sf::Texture* tex);
-    // Commit Crow - fin
-
-    // Commit Pixel-Perfect - debut
+  
     sf::Image normalImage;
     sf::Image ghostImage;
     const sf::Image* currentImage;
-    // Commit Pixel-Perfect - fin
     sf::Color skinColor = sf::Color::White;
 
 };
