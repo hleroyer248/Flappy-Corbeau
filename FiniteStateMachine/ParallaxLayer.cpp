d91340cd25f37972378d29599d248ba05f569cbb
hleroyer248 <hleroyer@gaming.tech>
2026-03-19 16:01:25 +0100

    Update ParallaxLayer.cpp

--- FiniteStateMachine/ParallaxLayer.cpp
+++ FiniteStateMachine/ParallaxLayer.cpp
@@ -3,18 +3,15 @@
 ParallaxLayer::ParallaxLayer(const sf::Texture& tex, float speedFactor)
     : factor(speedFactor), s1(tex), s2(tex)
 {
-    float scaleY = 1080.f * 0.928f / static_cast<float>(tex.getSize().y);
+    float scaleY = 1080.f / static_cast<float>(tex.getSize().y);
 
-    // On applique le même scale sur X pour garder les proportions de l'image
     float scaleX = scaleY;
 
     s1.setScale({ scaleX, scaleY });
     s2.setScale({ scaleX, scaleY });
 
-    // On calcule la largeur réelle du sprite redimensionné pour la boucle
     width = static_cast<float>(tex.getSize().x) * scaleX;
 
-    // Positionnement initial (0,0) car on remplit tout l'écran
     s1.setPosition({ 0.f, 0.f });
     s2.setPosition({ width, 0.f });
 }
