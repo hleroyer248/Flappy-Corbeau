12abc03e5132fd70e81e0b3c8ba2ad6f3fc7784a
hleroyer248 <hleroyer@gaming.tech>
2026-03-19 16:23:02 +0100

    Update CollisionBox.h

--- FiniteStateMachine/CollisionBox.h
+++ FiniteStateMachine/CollisionBox.h
@@ -5,10 +5,7 @@ class CollisionBox {
 public:
     CollisionBox();
     CollisionBox(const sf::FloatRect& rect);
-
-    // Commit Pixel-Perfect - debut
     CollisionBox(const sf::Sprite& sprite, const sf::Image& image);
-    // Commit Pixel-Perfect - fin
 
     void setRect(const sf::FloatRect& rect);
     const sf::FloatRect& getRect() const;
@@ -18,8 +15,6 @@ public:
 private:
     sf::FloatRect bounds;
 
-    // Commit Pixel-Perfect - debut
     const sf::Sprite* sprite;
     const sf::Image* image;
-    // Commit Pixel-Perfect - fin
 };
