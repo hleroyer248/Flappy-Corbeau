ac07c7a946fd8129aeb1c3ca2337d824ece6ca43
hleroyer248 <hleroyer@gaming.tech>
2026-03-19 17:50:59 +0100

    correction bug background

--- FiniteStateMachine/ParallaxLayer.cpp
+++ FiniteStateMachine/ParallaxLayer.cpp
@@ -3,17 +3,28 @@
 ParallaxLayer::ParallaxLayer(const sf::Texture& tex, float speedFactor)
     : factor(speedFactor), s1(tex), s2(tex)
 {
-    float scaleY = 1080.f / static_cast<float>(tex.getSize().y);
+    int cropLeft = 43;
+    int cropRight = 40;
 
+    int croppedWidth = tex.getSize().x - cropLeft - cropRight;
+    int height = tex.getSize().y;
+
+    sf::IntRect cropRect({ cropLeft, 0 }, { croppedWidth, height });
+
+    s1.setTextureRect(cropRect);
+    s2.setTextureRect(cropRect);
+
+    float scaleY = 1080.f / static_cast<float>(height);
     float scaleX = scaleY;
 
     s1.setScale({ scaleX, scaleY });
     s2.setScale({ scaleX, scaleY });
 
-    width = static_cast<float>(tex.getSize().x) * scaleX;
+    width = static_cast<float>(croppedWidth) * scaleX;
 
     s1.setPosition({ 0.f, 0.f });
-    s2.setPosition({ width, 0.f });
+
+    s2.setPosition({ width - 1.f, 0.f });
 }
 
 void ParallaxLayer::update(float dt, float baseSpeed)
@@ -22,12 +33,14 @@ void ParallaxLayer::update(float dt, float baseSpeed)
     s1.move({ -speed * dt, 0.f });
     s2.move({ -speed * dt, 0.f });
 
-    if (s1.getPosition().x <= -width) {
-        s1.setPosition({ s2.getPosition().x + width - 1.f, s1.getPosition().y });
+    float raccord = width - 1.f;
+
+    if (s1.getPosition().x <= -raccord) {
+        s1.setPosition({ s2.getPosition().x + raccord, s1.getPosition().y });
     }
 
-    if (s2.getPosition().x <= -width) {
-        s2.setPosition({ s1.getPosition().x + width - 1.f, s2.getPosition().y });
+    if (s2.getPosition().x <= -raccord) {
+        s2.setPosition({ s1.getPosition().x + raccord, s2.getPosition().y });
     }
 }
 
